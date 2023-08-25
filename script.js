b50a863a6e56a029abf012407d7593526fb1540a
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-25 01:07:55 -0400

    Create basic functions and buttons for calculator
    - Created basic arithmetic functions: add, subtract, multiply, divide.
    - Display content in HTML using JavaScript.
    - Developed an `operator` function that takes in an operator symbol and
      numbers.
    - Integrated existing `add`, `subtract`, `multiply`, and `divide`
      functions into the new `operator` function.

---
+++ script.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    alert("Cannot divide by zero!");
+    return "Error";
+  }
+  return a / b;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator, a, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else {
+    alert("Unsupported operator!");
+    return "Error";
+  }
+}
+
+let display = document.getElementById("display")
+display.textContent = "this is a display";
