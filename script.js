5593e883fbc453cd82bd45657301ad9b56230a3b
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-26 13:21:46 -0400

    Create delete button

--- script.js
+++ script.js
@@ -59,61 +59,84 @@ for (let i = 0; i <= 9; i++) {
   document.getElementById(`btn${i}`).addEventListener("click", function () {
     updateDisplay(i.toString());
   });
-};
+}
 
 let addition = document.getElementById("add").addEventListener("click", () => {
-    if (secondNumber !== "") {
-        firstNumber = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
-        secondNumber = "";
-      }
-  
-  
-    operator = "+";
+  if (secondNumber !== "") {
+    firstNumber = operate(
+      operator,
+      parseFloat(firstNumber),
+      parseFloat(secondNumber)
+    ).toString();
+    secondNumber = "";
+  }
+
+  operator = "+";
   display.textContent += " + ";
- 
 });
 let subtraction = document
   .getElementById("subtract")
   .addEventListener("click", () => {
     if (secondNumber !== "") {
-        firstNumber = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
-        secondNumber = "";
-      }
-   
+      firstNumber = operate(
+        operator,
+        parseFloat(firstNumber),
+        parseFloat(secondNumber)
+      ).toString();
+      secondNumber = "";
+    }
+
     operator = "-";
     display.textContent += "-";
-    
-});
+  });
 let multiplication = document
   .getElementById("multiply")
   .addEventListener("click", () => {
     if (secondNumber !== "") {
-        firstNumber = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
-        secondNumber = "";
-      }
-   
-   
+      firstNumber = operate(
+        operator,
+        parseFloat(firstNumber),
+        parseFloat(secondNumber)
+      ).toString();
+      secondNumber = "";
+    }
+
     operator = "*";
     display.textContent += "*";
-    
-});
-let division = document.getElementById("divide").addEventListener("click", () => {
+  });
+let division = document
+  .getElementById("divide")
+  .addEventListener("click", () => {
     operator = "/";
     display.textContent += "/";
+  });
 
+let clear = document.getElementById("clear").addEventListener("click", () => {
+  display.textContent = "";
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
 });
 
-let clear = document.getElementById("clear").addEventListener("click", () => { 
-    display.textContent = "";
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-   } );
-
-
-document.getElementById("equal").addEventListener("click", () => {
+let equal = document.getElementById("equal").addEventListener("click", () => {
   if (firstNumber !== "" && operator !== "" && secondNumber !== "") {
-    display.textContent = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-   
+    display.textContent = operate(
+      operator,
+      parseFloat(firstNumber),
+      parseFloat(secondNumber)
+    );
   }
+});
+
+let backspace = document.getElementById("backspace").addEventListener("click", () => {
+    if (secondNumber !== "") {
+        secondNumber = secondNumber.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+      } else if (operator !== "") {
+        operator = "";
+        display.textContent = display.textContent.slice(0, -3); // remove " + ", " - ", etc.
+      } else if (firstNumber !== "") {
+        firstNumber = firstNumber.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+      }   
 });
